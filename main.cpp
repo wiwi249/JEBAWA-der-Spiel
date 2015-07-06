db9a0b585a058cf2431109850feff2abf130dd65
wiwi249 <wiwi249@gmail.com>
2015-07-07 01:45:16 +0200

    powoli z biblioteką
    Wrzuciłem parę pierdółek z tej SFML już do maina :) Nie mam uprawnień do wrzucania więc tu pisze :/

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <string>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+#define VERSION "0.0"
+#define NAZWA "JEBAWKA-GAME"
+
 int main(int arg, char* argc[])
 {
-  std::cout<<"Szczesc Boze"<<std::endl;
+  string nazwa = str( format("%1% %2%") % NAZWA % VERSION );
+  sf::RenderWindow Okno( sf::VideoMode( 800, 600, 32 ), nazwa );
+  //std::cout<<"Szczesc Boze"<<std::endl;
   return 0;
 }
+
+//NIE WIEM CZO JA TU ZROBIŁEM XDD SPRAWDZE ZARA WIĘC SIE NIE PLUĆ HAKJERY.
